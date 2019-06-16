e0166d2d1d2f3ce71a6c2b5275c23cac4477b91e
Zaheer.Abbas <Zaheer.Abbas@CFTL-56.ccubefintech.com>
2019-06-16 20:52:28 +0530

    Create new Test Suite for Calculator

---
+++ API_Project/Test Suites/Calculator.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Calculator</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ae7f1ed9-a034-4f27-95a9-463c988ad288</testSuiteGuid>
+   <testCaseLink>
+      <guid>4d73ed5e-5993-4466-aeb7-ec9660eb4282</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition/Addition</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
