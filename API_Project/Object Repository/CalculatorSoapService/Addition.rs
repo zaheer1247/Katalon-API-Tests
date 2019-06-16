f72e290824db9a69802d65d4f04fb4f957809d52
Zaheer.Abbas <Zaheer.Abbas@CFTL-56.ccubefintech.com>
2019-06-16 13:36:01 +0530

    Separate folders for Rest and SOAP

--- API_Project/Object Repository/CalculatorSoapService/Addition.rs
+++ API_Project/Object Repository/CalculatorSoapService/Addition.rs
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
-   <description></description>
-   <name>getBank</name>
+   <description>Sample calculator Soap Addition request</description>
+   <name>Addition</name>
    <tag></tag>
-   <elementGuidId>2a915df9-3c98-4b3f-856d-7bf302c12d06</elementGuidId>
+   <elementGuidId>147f7a3c-2a38-41bd-95e4-f12af9352d0a</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <followRedirects>false</followRedirects>
@@ -13,25 +13,17 @@
    <restRequestMethod></restRequestMethod>
    <restUrl></restUrl>
    <serviceType>SOAP</serviceType>
-   <soapBody>POST /Quotation HTTP/1.0
-Host: www.xyz.org
-Content-Type: text/xml; charset = utf-8
-Content-Length: nnn
-
-&lt;?xml version = &quot;1.0&quot;?>
-&lt;SOAP-ENV:Envelope
-   xmlns:SOAP-ENV = &quot;http://www.w3.org/2001/12/soap-envelope&quot;
-   SOAP-ENV:encodingStyle = &quot;http://www.w3.org/2001/12/soap-encoding&quot;>
-
-   &lt;SOAP-ENV:Body xmlns:m = &quot;http://www.xyz.org/quotations&quot;>
-      &lt;m:GetQuotation>
-         &lt;m:QuotationsName>MiscroSoft&lt;/m:QuotationsName>
-      &lt;/m:GetQuotation>
-   &lt;/SOAP-ENV:Body>
-&lt;/SOAP-ENV:Envelope></soapBody>
+   <soapBody>&lt;Envelope xmlns=&quot;http://schemas.xmlsoap.org/soap/envelope/&quot;>
+    &lt;Body>
+        &lt;Add xmlns=&quot;http://tempuri.org/&quot;>
+            &lt;intA>4&lt;/intA>
+            &lt;intB>5&lt;/intB>
+        &lt;/Add>
+    &lt;/Body>
+&lt;/Envelope></soapBody>
    <soapHeader></soapHeader>
-   <soapRequestMethod>SOAP12</soapRequestMethod>
-   <soapServiceFunction>getBank</soapServiceFunction>
+   <soapRequestMethod>SOAP</soapRequestMethod>
+   <soapServiceFunction>Add</soapServiceFunction>
    <verificationScript>import static org.assertj.core.api.Assertions.*
 
 import com.kms.katalon.core.testobject.RequestObject
@@ -45,6 +37,9 @@ import internal.GlobalVariable as GlobalVariable
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
 ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
-</verificationScript>
-   <wsdlAddress></wsdlAddress>
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
+   <wsdlAddress>http://www.dneonline.com/calculator.asmx?WSDL</wsdlAddress>
 </WebServiceRequestEntity>
--- API_Project/Object Repository/SOAP/getBank.rs
+++ API_Project/Object Repository/SOAP/getBank.rs
@@ -23,5 +23,19 @@
    <soapHeader></soapHeader>
    <soapRequestMethod>SOAP</soapRequestMethod>
    <soapServiceFunction>getBank</soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
    <wsdlAddress>http://www.thomas-bayer.com/axis2/services/BLZService?wsdl</wsdlAddress>
 </WebServiceRequestEntity>
